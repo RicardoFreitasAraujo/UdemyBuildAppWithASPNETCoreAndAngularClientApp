b4cc70196afc9cc1818566343a618d5db6bbb9fa
Ricardo Freitas Araujo <ricardo.freitas@prestadores.uhgbrasil.com.br>
2019-04-04 10:46:08 -0300

    Modulo 11 Aula 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
@@ -58,7 +59,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     BsDropdownModule.forRoot(),
     RouterModule.forRoot(AppRoutes),
     TabsModule.forRoot(),
-    NgxGalleryModule
+    NgxGalleryModule,
+    FileUploadModule
   ],
   providers: [
             AuthService, 
--- src/app/members/photo-editor/photo-editor.component.ts
+++ src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PathLocationStrategy } from '@angular/common';
 import { Photo } from 'src/app/_models/photo';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -10,10 +13,30 @@ import { Photo } from 'src/app/_models/photo';
 export class PhotoEditorComponent implements OnInit {
 
   @Input() photos: Photo[];
-
-  constructor() { }
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+ 
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
+  }
+
+  public fileOverBase(e: any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['iamge'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    }); 
   }
 
 }
