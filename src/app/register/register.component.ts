99c6c1bd8489cf31f745ccfece618f507a433b72
Ricardo Freitas Araujo <ricardo.freitas@prestadores.uhgbrasil.com.br>
2019-04-05 15:02:22 -0300

    Moduo 12 Aula 04

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -22,7 +22,11 @@ export class RegisterComponent implements OnInit {
       username: new FormControl('sdsd', Validators.required ),
       password: new FormControl('', [ Validators.required,Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl('',[Validators.required])
-    });
+    }, this.passwordMatchValidator);
+  }
+
+  public passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
   public register() {
