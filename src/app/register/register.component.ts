15b58f5c1a3c4fe9ecd919f5af11b3827fa8b0ea
Ricardo Freitas Araujo <ricardo.freitas@prestadores.uhgbrasil.com.br>
2019-04-05 15:43:51 -0300

    Modulo 12 Aula 6

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { error } from 'util';
 import { AlertaService } from '../_services/alerta.service';
-import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -15,14 +15,20 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
 
-  constructor(private authService: AuthService,private alerta: AlertaService) { }
+  constructor(private authService: AuthService,
+              private alerta: AlertaService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required ),
-      password: new FormControl('', [ Validators.required,Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('',[Validators.required])
-    }, this.passwordMatchValidator);
+    this.createRegisterForm();
+  }
+
+  public createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['',[Validators.required,Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordMatchValidator });
   }
 
   public passwordMatchValidator(g: FormGroup) {
